0213b62b96f247a69fa699fb6f9fe1f659682681
tuyendv <tuyend@gmail.cc>
2022-05-29 19:50:06 +0700

    FIX header

--- layouts/default-layout/components/header.tsx
+++ layouts/default-layout/components/header.tsx
@@ -7,6 +7,7 @@ import { shortenAddress } from '../../../lib/helpers/utils'
 import AccountModal from '../../../components/AccountModal'
 import { CASH_ADDRESS, COW_ADDRESS } from '../../../constants'
 import Moralis from 'moralis'
+import Select from 'react-select'
 
 const DECIMALS = 5
 
@@ -33,12 +34,11 @@ export function Header({ ...props }) {
 
   return (
     <>
-      <div className="fixed backdrop-blur-lg top-0 z-10 border-b dark:border-[#292929] bg-opacity-60 dark:bg-opacity-50 w-full h-18 dark:bg-[#1a1a1a] bg-white px-2 md:px-[15px] flex items-center justify-between shadow">
+      <div className="fixed backdrop-blur-lg top-0 z-10 border-b dark:border-[#292929] bg-opacity-100 w-full h-18 dark:bg-[#101111] bg-white px-2 md:px-[15px] flex items-center justify-between shadow">
         <div className="flex items-center justify-start">
           <i
-            className={`text-[24px] cursor-pointer text-gray-700 dark:text-white transition-all duration-300 transform ${
-              props.isOpenMenu ? 'rotate-90' : 'rotate-0'
-            }`}
+            className={`text-[24px] cursor-pointer text-gray-700 dark:text-white transition-all duration-300 transform ${props.isOpenMenu ? 'rotate-90' : 'rotate-0'
+              }`}
             onClick={() => props?.openMenu()}
             style={{ transform: props.isOpenMenu ? 'rotate(180deg)' : '' }}
           >
@@ -54,7 +54,15 @@ export function Header({ ...props }) {
             className="ml-6 w-28"
           />
         </div>
-        <div className="flex">aaaa</div>
+        <div className="flex">
+          <form action="https://gategame.io/search.html" id="form-search">
+            <div className="flex items-center gap-4 p-2 px-4 bg-grayBg rounded focus:border-2 focus:border-blue-500">
+              <img src='/images/icon/search.png' className='w-4 h-4 cursor-pointer opacity-80 transition-opacity hover:opacity-100'/>
+              <input type="text" className="w-96 h-auto bg-transparent text-white" name="q" placeholder="Search games..." />
+              <a href="#"><i className="far fa-search"></i></a>
+            </div>
+          </form>
+        </div>
         <div className="flex items-center justify-center gap-x-4">
           {/* {isAuthenticated && user?.attributes?.accounts?.[0] ? (
             <Button
@@ -68,8 +76,13 @@ export function Header({ ...props }) {
               </div>
             </Button>
           ) : ( */}
-          <div className="mb-3 xl:w-30">
-            <select
+          <div className=" xl:w-30">
+            <Select
+              className="my-react-select-container flex-1 bg-transparent"
+              classNamePrefix="my-react-select"
+              defaultValue={{ value: 0, label: "English" }}
+              options={[{ value: 0, label: "English" }, { value: 1, label: "Vietnamese" }]} />
+            {/* <select
               className="form-select appearance-none
                 block
                 w-full
@@ -89,7 +102,7 @@ export function Header({ ...props }) {
             >
               <option value="2">English</option>
               <option value="3">VietNamese</option>
-            </select>
+            </select> */}
           </div>
           <Button
             primary
@@ -112,7 +125,7 @@ export function Header({ ...props }) {
         isOpen={openAccountModal}
         onClose={() => setOpenAccountModal(false)}
         address={''}
-        logout={() => {}}
+        logout={() => { }}
       />
     </>
   )
